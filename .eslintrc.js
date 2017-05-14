2742ca0261ac05e5cd66e79d2159a650077dd7e9
Franco Risso <franco.daniel.risso@zalando.de>
2017-05-14 20:16:39 +0200

    Improve code, add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  "extends": "airbnb",
+  "rules": {
+    "no-cond-assign": ["error", "except-parens"],
+    "no-use-before-define": "off",
+    "no-restricted-syntax": "off"
+  },
+  "plugins": [
+    "react",
+    "jsx-a11y",
+    "import"
+  ]
+};
--- index.js
+++ index.js
@@ -1,19 +1,18 @@
-const vm = require('vm');
-const loaderUtils = require("loader-utils");
+const loaderUtils = require('loader-utils');
 
-const converter = require("./converter");
+const converter = require('./src/converter');
 
-module.exports = function(source, map) {
-    const options = Object.assign({}, {
-        ignore: null,
-        target: null,
-    }, loaderUtils.getOptions(this));
+module.exports = function cssThemesLoader(source, map) {
+  const options = Object.assign({}, {
+    ignore: null,
+    target: null,
+  }, loaderUtils.getOptions(this));
 
-    if (!options.ignore && !options.target) {
-        this.callback(null, source, map);
-    }
+  if (!options.ignore && !options.target) {
+    this.callback(null, source, map);
+  }
 
-    const newText = converter({ text: source, target: options.target, ignore: options.ignore });
+  const newText = converter({ text: source, target: options.target, ignore: options.ignore });
 
-    this.callback(null, newText, map);
+  this.callback(null, newText, map);
 };
